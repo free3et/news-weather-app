c218eb74bb2f43e34fff16d87ff88559facacb09
free3et <free3et@gmail.com>
2023-07-10 13:01:41 +0300

    upload changes

--- app/components/NewsComponent/NewsComponent.tsx
+++ app/components/NewsComponent/NewsComponent.tsx
@@ -3,31 +3,35 @@ import UserAvatar from "../../assets/news/user_avatar.png";
 import SourseNews from "../../assets/news/earth_internet_browser.png";
 import Calendar from "../../assets/news/calendar_date.png";
 import DefaultImg from "../../assets/news/default_image.jpg";
+import Image from "next/image";
 
 export const NewsComponent = ({ post, className }) => {
   const { title, description, publishedAt, author, source, url, urlToImage } = post;
+  const externaImageLoader = ( urlToImage: string ) : string =>
+  `${urlToImage}`;
+
 
   const formatDate = publishedAt.slice(0, -4).split("T").join(" ");
   return (
     <article className={`${styles.newsCard_wrap} ${className}`}>
-      <div className={styles.newsCard_thumb_img}>
-        <img src={urlToImage || DefaultImg} alt={title} />
-      </div>
-      <div className={styles.newsCard_body}>
+      {/* <div className={styles.newsCard_thumb_img}>
+        <Image loader={externaImageLoader} src={urlToImage} alt={title} width='100' height='100' priority/>
+      </div> */}
+       <div className={styles.newsCard_body}>
         <h3>{title}</h3>
         <p className={styles.description}>{description || "no description"}</p>
         <div className={styles.userInfo_wrapper}>
           <div className={`${styles.userName_info}`}>
-            <img src={UserAvatar} alt={title} />
-            <p>{author || "no author"}</p>
+            <Image src={UserAvatar} alt={title} width='100' height='100'/>
+             <p>{author || "no author"}</p>
           </div>
           <div className={`${styles.sourceNews_info}`}>
-            <img src={SourseNews} alt={title} />
+            <Image src={SourseNews} alt={title} width='100' height='100'/>
             <a href={url}>{source.name.toLowerCase()}</a>
           </div>
 
           <div className={styles.userDate_info}>
-            <img src={Calendar} alt={title} />
+            <Image src={Calendar} alt={title} width='100' height='100'/>
             <p>{formatDate}</p>
           </div>
         </div>
