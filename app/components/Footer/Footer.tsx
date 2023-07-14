d530f7c31a54cd0d5458e6dd9526b708d2fced5f
free3et <free3et@gmail.com>
2023-07-14 11:34:59 +0300

    add header, footer, fix news fields

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -23,7 +23,7 @@ export const Footer = () => {
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2539.352383718622!2d30.51706346594457!3d50.47178324370834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cef1dad747f1%3A0x5c6494f5e9d8d874!2sItera!5e0!3m2!1sen!2sua!4v1672249419229!5m2!1sen!2sua"
             style={{ border: 0 }}
-            allowFullScreen=""
+            allowFullScreen={true}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
